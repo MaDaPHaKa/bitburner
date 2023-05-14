a3c5dd689941c244b68fceeb26cd13a0786c9960
M4D4PH4K4 <molino.luca@gmail.com>
2023-05-14 07:38:43 +0200

    runtime edits

--- src/const/servers.ts
+++ src/const/servers.ts
@@ -4,11 +4,11 @@ export const XP_FARMER_SERVER_PREFIX = 'farmer-srv-';
 export const HOME_FREE_RAM_TO_KEEP = 40;
 export const MIN_HOME_RAM_TO_RUN_HWGW = 1024;
 // MAX_RAM_STANDARD 1048576
-// MAX_RAM_CURRENT_BN 1048576
-export const MAX_RAM = 1048576;
-export const SERVER_GB = 1048576;
-export const SERVER_GB_WANTED = 1048576;
-export const MAX_SERVER_NUM = 25;
+// MAX_RAM_CURRENT_BN 524288
+export const MAX_RAM = 524288;
+export const SERVER_GB = 16384;
+export const SERVER_GB_WANTED = 16384;
+export const MAX_SERVER_NUM = 24;
 export const MAX_FARMER_SERVER_NUM = 0;
-export const FARM_SERVER_GB = 1048576;
+export const FARM_SERVER_GB = 524288;
 // 262144 | 131072 | 65536 | 32768 | 16384 | 8192
